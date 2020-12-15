b7df4fd3b569dc5f881ff73c30bc513ad231c7a9
Luke-kb <keananbrown@gmail.com>
2020-12-15 13:54:30 +1100

    changed to yarn. added packages for auth and nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportLocal = require('passport-local')
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
 
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
+// Middleware 
 app.use(cors())
 app.use(express.json())
 
 // Atlas connection
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(uri, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -26,6 +35,7 @@ const usersRouter = require('./routes/users')
 app.use('/bookings', bookingsRouter)
 app.use('/users', usersRouter)
 
+// run server 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
