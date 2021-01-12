75c755e873a2a0d7300568702a2ee993251ff38d
Luke-kb <keananbrown@gmail.com>
2021-01-12 11:18:50 +1100

    added correct baseURL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 // create axios instance 
-const axiosBaseURL = axios.create({
-    baseURL: "http://localhost:3000",  //change this to deployment server when at production stage
+export default axios.create({
+    baseURL: "http://localhost:5000",  //change this to deployment server when at production stage
     timeout: 5000
 })
-
-export default axiosBaseURL
