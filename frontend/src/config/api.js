584257ca27795d55974c5c5555c5176ff2875c2b
Luke-kb <keananbrown@gmail.com>
2021-01-13 15:32:00 +1100

    commented notes for booking/payment process. axios dependencies for CheckoutForm

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 // create axios instance 
 export default axios.create({
-    baseURL: "http://localhost:5000",  //change this to deployment server when at production stage
+    baseURL: "http://localhost:5000",  //change this to heroku deployment server when at production stage
     timeout: 5000
 })
