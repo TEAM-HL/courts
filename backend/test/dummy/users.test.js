eb13c717ffad638d66f610233284ed7005bf45ff
Luke-kb <keananbrown@gmail.com>
2021-01-17 13:39:13 +1100

    added test directory. created dummy user file. added mocha and chai to dev dependencies

---
+++ backend/test/dummy/users.test.js
@@ -0,0 +1,26 @@
+const users = [
+    
+    {
+        username: "billy3",
+        email: "billyboy@email.com",
+        password: "1234567AaA",
+        userType: "player",
+        signUpDate: "23/10/20"
+    },
+    {
+        username: "alice6",
+        email: "alice@email.com",
+        password: "ApplePie76",
+        userType: "player",
+        signUpDate: "10/10/20"
+    },
+    {
+        username: "bobby",
+        email: "bobby@email.com",
+        password: "BobbyBob8",
+        userType: "player",
+        signUpDate: "2/12/20"
+    }   
+]
+
+export default users
