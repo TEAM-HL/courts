3d4793703b18b74ed8da0ade5dd455b7bf97b6b2
Hugh-Charlton <62679235+Hugh-Charlton@users.noreply.github.com>
2021-01-18 16:09:03 +1100

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // create axios instance 
 export default axios.create({
     // https://radiant-shore-26863.herokuapp.com/
-    baseURL: "https://radiant-shore-26863.herokuapp.com/",  //change this to deployment server when at production stage
+    baseURL: "https://radiant-shore-26863.herokuapp.com",  //change this to deployment server when at production stage
     timeout: 5000
 })
 
