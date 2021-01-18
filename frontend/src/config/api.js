205ebf12b59bc5f8bdaf4fa69416db7e47c66c39
Luke-kb <keananbrown@gmail.com>
2021-01-19 09:50:33 +1100

    Update api.js
    added withCredentials, removed "/"

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // create axios instance 
 export default axios.create({
-    // https://radiant-shore-26863.herokuapp.com/
-    baseURL: "https://radiant-shore-26863.herokuapp.com/",  //change this to deployment server when at production stage
-    timeout: 5000
+    baseURL: "https://radiant-shore-26863.herokuapp.com", 
+    timeout: 5000,
+    withCredentials: true
 })
 
