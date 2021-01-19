f83c512678091cb4dbaece3bbf5806e38d975ab6
Luke-kb <keananbrown@gmail.com>
2021-01-19 21:28:57 +1100

    updated dashboard. cleanup

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -25,40 +25,40 @@ const Dashboard = () => {
                 <Slide image={<img alt="" src="https://d24lqeczfu7s1y.cloudfront.net/venue/meadowbank-park-tc/9c4dbe3c-7d0f-477b-bd74-719b8e90b437.jpg"/>}>
                     <Caption placement="center">
                     <h3>
-                        This is our big Tagline!
+                        Welcome to Courts
                     </h3>
                     <h5 className="light grey-text text-lighten-3">
-                        Here's our small slogan.
+                        To book a court, sign in above.
                     </h5>
                     </Caption>
                 </Slide>
                 <Slide image={<img alt="" src="https://d24lqeczfu7s1y.cloudfront.net/venue/meadowbank-park-tc/34c340d8-c2b3-45b7-a1a4-71f3b2854d1e.jpg"/>}>
                     <Caption placement="left">
                     <h3>
-                        Left Aligned Caption
+                        Accessible
                     </h3>
                     <h5 className="light grey-text text-lighten-3">
-                        Here's our small slogan.
+                        AAA
                     </h5>
                     </Caption>
                 </Slide>
                 <Slide image={<img alt="" src="https://d24lqeczfu7s1y.cloudfront.net/venue/meadowbank-park-tc/09bb834f-a648-481d-b299-5410004298c6.jpg"/>}>
                     <Caption placement="right">
                     <h3>
-                        Right Aligned Caption
+                        Affordable
                     </h3>
                     <h5 className="light grey-text text-lighten-3">
-                        Here's our small slogan.
+                        AAA
                     </h5>
                     </Caption>
                 </Slide>
-                <Slide image={<img alt="" src="https://images.unsplash.com/photo-1579873676155-6e6f5ab538a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"/>}>
+                <Slide image={<img alt="" src="https://images.unsplash.com/photo-1593838957161-7b18571c8c83?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"/>}>
                     <Caption placement="center">
                     <h3>
-                        This is our big Tagline!
+                        Achievable
                     </h3>
                     <h5 className="light grey-text text-lighten-3">
-                        Here's our small slogan.
+                        AAA
                     </h5>
                     </Caption>
                 </Slide>
