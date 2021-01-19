49065519fd0593aeae3cce8cbd015c53e5f48607
Luke-kb <keananbrown@gmail.com>
2021-01-19 11:27:09 +1100

    updated BaseURL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // create axios instance 
 export default axios.create({
-    baseURL: "https://radiant-shore-26863.herokuapp.com", 
+    baseURL: "https://polar-river-03280.herokuapp.com", 
     timeout: 5000,
     withCredentials: true
 })
