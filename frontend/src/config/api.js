bbae143360d8737b6ce891577dcc5a90017d019e
Luke-kb <keananbrown@gmail.com>
2021-01-19 18:45:58 +1100

    fixed baseURL. removed domain in cookie config

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 // create axios instance 
 export default axios.create({
-    // baseURL: "https://polar-river-03280.herokuapp.com", 
-    baseURL: "http://localhost:5000", 
+    baseURL: "https://polar-river-03280.herokuapp.com", 
     timeout: 5000,
     withCredentials: true
 })
